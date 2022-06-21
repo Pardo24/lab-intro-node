2d99eb4f967bd6ba93485941dc10e1f81ee87cfe
Dani Pardo <danielp.5@hotmail.com>
2022-06-21 16:25:55 +0200

    hecho

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= []
+    this.length= this.items.length;
+    
+  }
 
-  add(item) {}
+  add(item) {
+this.items.push(item);
+this.items.sort((a,b)=> a-b)
+this.length=this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos>this.items.length){
+      return OutOfBonds
+    }
+    else{
+return this.items[pos]}
+  }
 
-  max() {}
+  max() {
+    if(this.items.length=== 0){
+      return EmptySortedList
+    }
+    else{
+      return Math.max(...this.items)
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length=== 0){
+      return EmptySortedList
+    }
+    else{
+      return Math.min(...this.items)
+    }
+  }
 
-  sum() {}
+  sum() {
+    let suma=0
+    for(let i=0; i<this.items.length; i++){
+      suma+= this.items[i]
+    }
+    return suma
+  }
 
-  avg() {}
+  avg() {
+    let avg=0
+    if(this.items.length===0){
+      return EmptySortedList
+    }
+    else{
+    for(let i=0; i<this.items.length; i++){
+      avg+= this.items[i]
+    }
+    avg=avg/this.items.length
+    return avg}
+  }
 }
 
 module.exports = SortedList;
